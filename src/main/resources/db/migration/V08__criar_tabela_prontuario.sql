a31e4196d1d4a0af4a4a53a56256752879fc7380
Admin - Marcio Jose Lima de Sousa <a.marcio.sousa@mpt.intra>
2018-08-24 17:40:05 -0300

    AMSCLINICA-API 1.1.3

--- src/main/resources/db/migration/V08__criar_tabela_prontuario.sql
+++ src/main/resources/db/migration/V08__criar_tabela_prontuario.sql
@@ -1,9 +1,9 @@
 CREATE TABLE prontuario (
 	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
 	anexo VARCHAR(200),
-	codigo_paciente BIGINT(20),
-	codigo_medico BIGINT(20),
 	relatorio VARCHAR(400),
+	codigo_paciente BIGINT(20) NOT NULL,
+	codigo_medico BIGINT(20) NOT NULL,
 	FOREIGN KEY (codigo_paciente) REFERENCES paciente(codigo),
 	FOREIGN KEY (codigo_medico) REFERENCES medico(codigo)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
